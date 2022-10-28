e5748295c2623fa141fecab6ddef42ca4ec59862
KiritoU <nguyentienduongtink21@gmail.com>
2022-10-28 09:56:36 +0700

    add category status

--- helper.py
+++ helper.py
@@ -383,11 +383,23 @@ class Helper:
                 data=pmeta,
             )
 
+        if serie_details["status"].lower() == "ongoing":
+            database.insert_into(
+                table="term_relationships", data=(postId, CONFIG.ONGOING_TAXONOMY_ID, 0)
+            )
+
+        if serie_details["status"].lower() == "completed":
+            database.insert_into(
+                table="term_relationships",
+                data=(postId, CONFIG.COMPLETED_TAXONOMY_ID, 0),
+            )
+
         database.insert_into(table="term_relationships", data=(postId, 1, 0))
 
         # self.insert_taxonomy(postId, ["Japan"], "country")
         self.insert_taxonomy(postId, serie_details["released"], "release")
         self.insert_taxonomy(postId, serie_details["genre"], "genres")
+        # self.insert_taxonomy(postId, serie_details["status"], "status")
 
         return [postId, thumbId]
 
@@ -476,10 +488,22 @@ class Helper:
                 data=pmeta,
             )
 
+        if episode["status"].lower() == "ongoing":
+            database.insert_into(
+                table="term_relationships", data=(postId, CONFIG.ONGOING_TAXONOMY_ID, 0)
+            )
+
+        if episode["status"].lower() == "completed":
+            database.insert_into(
+                table="term_relationships",
+                data=(postId, CONFIG.COMPLETED_TAXONOMY_ID, 0),
+            )
+
         database.insert_into(table="term_relationships", data=(postId, 1, 0))
         # self.insert_taxonomy(postId, ["Japan"], "country")
         self.insert_taxonomy(postId, episode["released"], "release")
         self.insert_taxonomy(postId, episode["genre"], "genres")
+        # self.insert_taxonomy(postId, episode["status"], "status")
 
     def insert_serie(self, serie_details: dict):
         try:
@@ -492,6 +516,7 @@ class Helper:
             return
 
         for episode in serie_details["child_episode"]:
+            episode["status"] = serie_details["status"]
             try:
                 self.insert_serie_episode(episode, serieId, thumbId)
                 sleep(0.1)
